19e02f95d1e1edc4b8cc27cd505ead64c4a94f2e
Ansh Aggarwal <83965153+AnshAggarwal4015@users.noreply.github.com>
2023-08-29 04:40:36 +0530

    Cors Origin Changes

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || `https://studynotion-frontend-71fqp3qjx-anshaggarwal4015.vercel.app/`,
     credentials: true,
   })
 );
